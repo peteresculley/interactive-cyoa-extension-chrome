5b1899986dc94f2d3705e38005b17bb305ec8298
peteresculley <psculley3@gmail.com>
2023-12-21 11:11:06 +0100

    initial commit

---
+++ action/action.js
@@ -0,0 +1,140 @@
+const pointsContainer = document.getElementById('points-container');
+const actionsContainer = document.getElementById('actions-container');
+
+chrome.runtime.onMessageExternal.addListener(async (message, sender, sendResponse) => {
+  const tab = await getCurrentTab();
+
+  if (!tab || !sender.tab || sender.tab.id === tab.id) {
+    if (actionsContainer.style.display !== 'flex') {
+      actionsContainer.style.display = 'flex';
+    }
+    switch (message.type) {
+      case 'points':
+        updatePoints(message.points);
+        break;
+    }
+  }
+});
+
+function updatePoints(points) {
+  for (let i = 0; i < points.length; i++) {
+    const point = points[i];
+
+    let child;
+    if (i < pointsContainer.childNodes.length) {
+      child = pointsContainer.childNodes[i];
+    } else {
+      child = document.createElement('div');
+      child.className = 'point';
+      const nameElem = document.createElement('div');
+      nameElem.className = 'name';
+      const valueElem = document.createElement('input');
+      valueElem.className = 'value';
+      const index = i;
+      valueElem.onchange = () => {
+        try {
+          const value = parseFloat(valueElem.value);
+          getCurrentTab().then((tab) => {
+            chrome.scripting.executeScript({
+              target: { tabId: tab.id },
+              func: updatePoint,
+              args: [index, value],
+              world: chrome.scripting.ExecutionWorld.MAIN
+            });
+          });
+        } catch(e) {}
+      };
+      child.appendChild(nameElem);
+      child.appendChild(valueElem);
+      pointsContainer.appendChild(child);
+    }
+
+    const nameElem = child.querySelector('.name');
+    const valueElem = child.querySelector('.value');
+    if (nameElem.innerText !== point.name) {
+      nameElem.innerText = point.name;
+    }
+    if (valueElem.value !== point.value && valueElem !== document.activeElement) {
+      valueElem.value = point.value;
+    }
+  }
+
+  for (let i = pointsContainer.childNodes.length - 1; i >= points.length; i--) {
+    pointsContainer.removeChild(pointsContainer.childNodes[i]);
+  }
+}
+
+function updatePoint(index, value) {
+  try {
+    (() => {
+      const app = document.querySelector('#app').__vue__.$store.state.app;
+
+      app.pointTypes[index].startingSum = value;
+    })()
+  } catch(e) {}
+}
+
+document.getElementById('remove-row-limits-button').onclick = async () => {
+  try {
+    await chrome.scripting.executeScript({
+      target: { tabId: (await getCurrentTab()).id },
+      func: removeRowLimits,
+      world: chrome.scripting.ExecutionWorld.MAIN
+    });
+  } catch(e) {}
+};
+
+function removeRowLimits() {
+  try {
+    (() => {
+      const app = document.querySelector('#app').__vue__.$store.state.app;
+    
+      function allThings(func) {
+        app.rows.forEach((row) => allObjects(row, func));
+      }
+      function allObjects(row, func) {
+        func(row);
+        if (row.objects && row.objects.length) {
+          row.objects.forEach((row) => allObjects(row, func));
+        }
+      }
+      allThings((obj) => obj.allowedChoices = 0);
+    })();
+  } catch (e) {}
+}
+
+document.getElementById('remove-requirements-button').onclick = async () => {
+  try {
+    await chrome.scripting.executeScript({
+      target: { tabId: (await getCurrentTab()).id },
+      func: removeRequirements,
+      world: chrome.scripting.ExecutionWorld.MAIN
+    });
+  } catch (e) {}
+};
+
+function removeRequirements() {
+  try {
+    (() => {
+      const app = document.querySelector('#app').__vue__.$store.state.app;
+
+      function allThings(func) {
+        app.rows.forEach((row) => allObjects(row, func));
+      }
+      function allObjects(row, func) {
+        func(row);
+        if (row.objects && row.objects.length) {
+          row.objects.forEach((row) => allObjects(row, func));
+        }
+      }
+      allThings((obj) => obj.requireds ? obj.requireds = [] : undefined);
+    })();
+  } catch (e) {}
+}
+
+async function getCurrentTab() {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
---
+++ background/service-worker.js
@@ -0,0 +1,60 @@
+chrome.runtime.onInstalled.addListener(async () => {
+  await chrome.action.disable();
+  await chrome.action.setBadgeBackgroundColor(
+    { color: '#FFFFFF' }
+  );
+});
+
+chrome.webNavigation.onCompleted.addListener(async (details) => {
+  try {
+    await chrome.scripting.executeScript({
+      target: { tabId: details.tabId },
+      func: pageScript,
+      args: [chrome.runtime.id],
+      world: chrome.scripting.ExecutionWorld.MAIN
+    });
+  } catch (e) {}
+});
+
+chrome.runtime.onMessageExternal.addListener(async (message, sender, sendResponse) => {
+  const tabId = sender.tab.id;
+
+  switch (message.type) {
+    case 'activate':
+      await chrome.action.enable(tabId);
+      await chrome.action.setBadgeText({ text: 'ON', tabId });
+      break;
+  }
+});
+
+function pageScript(extId) {
+  try {
+    (() => {
+      const attachInterval = setInterval(() => {
+        try {
+          const app = document.querySelector('#app').__vue__.$store.state.app;
+          if (!app) return;
+    
+          clearInterval(attachInterval);
+          chrome.runtime.sendMessage(extId, { type: 'activate' });
+    
+          setInterval(updateScores, 500);
+        } catch(e) {}
+      }, 1000);
+    
+      setTimeout(() => clearInterval(attachInterval), 5 * 60 * 1000); // stop trying after 5 minutes
+    
+      function updateScores() {
+        const app = document.querySelector('#app').__vue__.$store.state.app;
+        if (!app) return;
+    
+        const points = app.pointTypes.map((point) => ({
+          name: point.name,
+          value: point.startingSum
+        }));
+    
+        chrome.runtime.sendMessage(extId, { type: 'points', points });
+      }
+    })();  
+  } catch (e) {}
+}
